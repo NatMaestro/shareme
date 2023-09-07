9ebe922a4b10eb3d83ad3da3c8afe7afdfec6b76
Nathaniel <nathanielguggisberg@gmail.com>
2023-09-07 20:00:11 +0100

    first commit

---
+++ schemas/user.js
@@ -0,0 +1,29 @@
+export default{
+    name:'user',
+    title:'User',
+    type:'document',
+    fields:[
+        {
+            name:'userName',
+            title: 'UserName',
+            type: 'string'
+        },
+        {
+            name:'image',
+            title: 'Image',
+            type: 'string'
+        },
+        {
+            name:'googleID',
+            title: 'googleID',
+            type: 'string'
+        },
+        {
+            name:'loggedIn',
+            title: 'LoggedIn',
+            type: 'boolean'
+        },
+
+    ]
+    
+}
